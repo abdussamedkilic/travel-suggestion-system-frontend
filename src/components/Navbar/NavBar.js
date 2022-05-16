ef9e62ce9f7809377efa5480f0015a9813c61434
AbdussamedKILIC <62799218+AbdussamedKILIC@users.noreply.github.com>
2022-05-16 10:43:32 +0300

    navbar style v2

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -3,22 +3,33 @@ import React from 'react';
 
 export const NavBar = () => {
     return (
-        <div className="nav">
-            <input type="checkbox" id="nav-check"></input>
-            <div className="nav-header">
-                <div className="nav-title">Travel Suggestion System</div>
-            </div>
-            <div className="nav-btn">
-                <label htmlFor="nav-check">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </label>
-            </div>
-            <div className="nav-links">
-                <a href="/">Home</a>
-                <a href="/suggestion">Suggestions</a>
-            </div>
-        </div>
+        <nav role="navigation">
+            <ul>
+                <li>
+                    <a href="/">
+                        <div className="Logo">Travel Suggestion System</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="/Cities" aria-haspopup="true">
+                        Two
+                    </a>
+                    <ul class="dropdown" aria-label="submenu">
+                        <li>
+                            <a href="/cities1">Sub-1</a>
+                        </li>
+                        <li>
+                            <a href="/cities2">Sub-2</a>
+                        </li>
+                        <li>
+                            <a href="/cities3">Sub-3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/suggestion">Three</a>
+                </li>
+            </ul>
+        </nav>
     );
 };
