608591cc5d8b132e961acceb732d734ae6eb83a5
AbdussamedKILIC <62799218+AbdussamedKILIC@users.noreply.github.com>
2022-05-17 09:44:50 +0300

    update navbar

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,35 +1,59 @@
 import './NavBar.scss';
 import React from 'react';
+import suitcase from '../../images/svgs/suitcase.svg';
 
 export const NavBar = () => {
+    const Logo = () => {
+        return (
+            <div>
+                <a href="/">
+                    <div className="logo-wrap">
+                        <div id="logo" className="logo" alt="Brand logo">
+                            <img
+                                src={suitcase}
+                                alt="suitcase"
+                                width="100"
+                                height="40"
+                            />
+                        </div>
+                    </div>
+                </a>
+            </div>
+        );
+    };
+
+    const Navbar = () => {
+        return (
+            <nav>
+                <ul className="navbar">
+                    <li>
+                        <a href="/suggestion">All services</a>
+                    </li>
+                    <li>
+                        <a href="#">Dropdown menu cities</a>
+                    </li>
+                    <li>
+                        <a href="#"> something else</a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    };
+
+    const Header = () => {
+        return (
+            <header>
+                <div className="h-container">
+                    <Logo />
+                    <Navbar />
+                </div>
+            </header>
+        );
+    };
+
     return (
-        <nav role="navigation">
-            <ul>
-                <li>
-                    <a href="/">
-                        <div className="Logo">Travel Suggestion System</div>
-                    </a>
-                </li>
-                <li>
-                    <a href="/Cities" aria-haspopup="true">
-                        Two
-                    </a>
-                    <ul class="dropdown" aria-label="submenu">
-                        <li>
-                            <a href="/cities1">Sub-1</a>
-                        </li>
-                        <li>
-                            <a href="/cities2">Sub-2</a>
-                        </li>
-                        <li>
-                            <a href="/cities3">Sub-3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="/suggestion">Three</a>
-                </li>
-            </ul>
-        </nav>
+        <>
+            <Header />
+        </>
     );
 };
